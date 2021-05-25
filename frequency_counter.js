e0a760515e5d84db9aa49cea9b5e350fe8888c2e
frogr <austindanielfrench@gmail.com>
2021-05-25 12:53:44 -0700

    frequency counter example

--- frequency_counter.js
+++ frequency_counter.js
@@ -1,4 +1,5 @@
-function same(arr1, arr2) {
+// my intial solution
+function _same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return console.log(false)
   }
@@ -12,6 +13,31 @@ function same(arr1, arr2) {
     return console.log(false)
   }
 }
+
+// refactored solution w/o sort
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return console.log(false)
+  }
+  let frequencyCounter1 = {}
+  let frequencyCounter2 = {} 
+  for (let i of arr1) {
+    frequencyCounter1[i] = (frequencyCounter1[i] || 0) + 1
+  }
+  for (let i of arr2) {
+    frequencyCounter2[i] = (frequencyCounter2[i] || 0) + 1
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return console.log(false)
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return console.log(false)
+    }
+  }
+  return console.log(true);
+}
+// tests
 same([1, 2, 3], [1, 9]) // false
 same([1, 2, 2, 5, 3], [1, 4, 4, 25, 9]) // true 
 same([1, 2, 1], [4, 4, 1]) // false
