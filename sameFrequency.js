7308b12afa43542ad5d33836c36a7fcd2924d032
frogr <austindanielfrench@gmail.com>
2021-05-28 19:08:17 -0700

    not sure why this isnt passing the test

--- sameFrequency.js
+++ sameFrequency.js
@@ -3,20 +3,24 @@ function sameFrequency(num1, num2) {
   let fc2 = {}
   const num1Arr = String(num1).split("")
   const num2Arr = String(num2).split("")
+
   for (let i of num1Arr) {
     (fc1[i] > 0) ? fc1[i]++ : fc1[i] = 1
   }
   for (let j of num2Arr) {
     (fc2[j] > 0) ? fc2[j]++ : fc2[j] = 1
   }
+
   let vals1 = Object.values(fc1)
   let vals2 = Object.values(fc2)
+  if (vals1.length !== vals2.length) return false
+
   for (let i = 0; i < vals1.length; i++) {
     if (vals1[i] != vals2[i]) {
-      return console.log(false)
+      return false
     }
   }
-  return console.log(true)
+  return true
 }
 
 sameFrequency(182, 281) // t
